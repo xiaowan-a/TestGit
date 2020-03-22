53c47ba07858c78e41df5f8e9dd5d153ab3538b1
xiaowan <wan15319111484@163.com>
2020-03-23 01:06:35 +0800

    edit student

--- src/main/java/com/baidu/test/Student.java
+++ src/main/java/com/baidu/test/Student.java
@@ -3,7 +3,8 @@ package com.baidu.test;
 public class Student {
 
 	public static void main(String[] args){
-		System.out.println("left Join!");
+		System.out.println("left Join and join");
 	}
 	
 }
+ 
